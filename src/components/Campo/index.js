a1697dbec2cb8108bf3cdde71a238d2d40fe40c3
SauloAndres1982 <saulo.carmona@soyrosarista.edu.co>
2024-11-24 17:03:12 -0500

    componente button

--- src/components/Campo/index.js
+++ src/components/Campo/index.js
@@ -5,7 +5,7 @@ const Campo = (props) => {
     return(
         <div className="campo-texto">
             <label>{props.titulo}</label>
-            <input placeholder={placeholderModificado}/>
+            <input placeholder={placeholderModificado} required/>
         </div>
     )
 }
--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -1,18 +1,27 @@
 import "./Formulario.css";
 import Campo from "../Campo";
 import ListaOpciones from "../ListaOpciones/ListaOpciones";
+import Button from "../Button/Button";
+
 
 
 function Formulario(){
+
+    const controlarEnvio = (evento) => {
+        console.log("Controlar envío", evento)
+        evento.preventDefault()
+    }
     return(
         <section className="formulario">
-            <form>
+            <form onSubmit={controlarEnvio}>
                 <h2>Rellena el formulario para crear el colaborador.</h2>
                 <Campo titulo="Nombre" placeholder="Ingresar nombre"/>
                 <Campo titulo="Puesto" placeholder="Ingresar puesto"/>
                 <Campo titulo="Foto" placeholder="Ingresar foto"/>
                 <ListaOpciones />
-                <button type="submit">Crear</button>
+                <Button>
+                    Crear
+                </Button>
             </form>
         </section>
     )
